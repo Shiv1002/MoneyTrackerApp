0579c83e1492672fe86789532205203906e88cc9
shivank <shivank.pasi@somaiya.edu>
2024-06-07 19:16:25 +0530

    update views dir setting

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(
 const PORT = process.env.PORT || 1002;
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(flash());
 
